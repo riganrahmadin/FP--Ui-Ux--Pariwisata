ee1fb4858d42ff4446afec3cc04501cb82019587
riganrahmadin <riganrahmadin8@gmail.com>
2023-07-03 02:41:48 +0700

    fix script google

--- application/views/details/detailsScript.php
+++ application/views/details/detailsScript.php
@@ -1,3 +1,5 @@
 <script src="public/owlcarousel/owl.carousel.min.js"></script>
 <script type="text/javascript" src="public/js/owlDetails.js"></script>
 <script src="public/js/getMaps.js"></script>
+<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1ptVPqYSmsM12g1ckjt3EL6WzA_A1SWM&callback=initMap">
+</script>
--- public/js/getMaps.js
+++ public/js/getMaps.js
@@ -3,9 +3,10 @@ let map;
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -7.7827623221833875, lng: 110.36703275776573},
     zoom: 8,
   });
 }
 
 initMap();
+
